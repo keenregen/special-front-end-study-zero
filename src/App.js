42e432b40de60f84831ee542fdf7a9c0d33d55db
Keen Regen <keenregen@gmail.com>
2023-03-13 06:21:46 +0300

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Header, HeaderTitle } from "./Header0";
 
 function App() {
   const [headerTitleColor, setHeaderTitleColor] = useState("blue");
   const [showDummyInfo, setShowDummyInfo] = useState(true);
+  const [firstRender, setFirstRender] = useState(true);
 
   const fnClick = () => {
     headerTitleColor === "blue"
       ? setHeaderTitleColor("white")
       : setHeaderTitleColor("blue");
+    setFirstRender(false);
   };
+
+  useEffect(() => {
+    if (firstRender === true) return;
+    setShowDummyInfo(false);
+  }, [firstRender, headerTitleColor]);
 
   return (
     <div className="App">
